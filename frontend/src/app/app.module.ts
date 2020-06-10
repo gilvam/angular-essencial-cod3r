6ba18acda16510c9225a29938d11d03de499f984
Gilvam Mourão <gilvamm@gmail.com>
2020-06-10 15:20:12 -0300

    Adicionado component home

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -11,13 +11,16 @@ import { NavComponent } from './components/template/nav/nav.component';
 import { MatSidenavModule } from "@angular/material/sidenav";
 import { MatListModule } from "@angular/material/list";
 import { BrowserAnimationsModule, NoopAnimationsModule } from "@angular/platform-browser/animations";
+import { HomeComponent } from './views/home/home.component';
+import { MatCardModule } from "@angular/material/card";
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    NavComponent
+    NavComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
@@ -26,7 +29,8 @@ import { BrowserAnimationsModule, NoopAnimationsModule } from "@angular/platform
     AppRoutingModule,
     MatToolbarModule,
     MatSidenavModule,
-    MatListModule
+    MatListModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
