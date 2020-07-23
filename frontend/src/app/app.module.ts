e0c95088405bb4f9e4e49a727b91677e9c877213
Gilvam Mourão <gilvamm@gmail.com>
2020-07-23 15:10:57 -0300

    Adicionado listagem de produtos

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserAnimationsModule, NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
@@ -24,45 +24,58 @@ import { NavComponent } from './components/template/nav/nav.component';
 import { HomeComponent } from './views/home/home.component';
 import { ProductCrudComponent } from './views/product-crud/product-crud.component';
 import { ProductCreateComponent } from './components/product/product-create/product-create.component';
+import { ProductReadComponent } from './components/product/product-read/product-read.component';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
+import localePt from '@angular/common/locales/pt';
+import { registerLocaleData } from '@angular/common';
+
+registerLocaleData(localePt);
 
 @NgModule({
-  declarations: [
-    // components
-    AppComponent,
-    HeaderComponent,
-    FooterComponent,
-    NavComponent,
-    HomeComponent,
-    ProductCrudComponent,
-    ProductCreateComponent,
+	declarations: [
+		// components
+		AppComponent,
+		HeaderComponent,
+		FooterComponent,
+		NavComponent,
+		HomeComponent,
+		ProductCrudComponent,
+		ProductCreateComponent,
 
-    // directives
-    RedDirective,
-    ForDirective,
+		// directives
+		RedDirective,
+		ForDirective,
+		ProductReadComponent,
 
-    // pipes
-  ],
-  imports: [
-    BrowserModule,
-    BrowserAnimationsModule,
-    NoopAnimationsModule,
-    AppRoutingModule,
-    HttpClientModule,
-    FormsModule,
+		// pipes
+	],
+	imports: [
+		BrowserModule,
+		BrowserAnimationsModule,
+		NoopAnimationsModule,
+		AppRoutingModule,
+		HttpClientModule,
+		FormsModule,
 
-    // material
-    MatToolbarModule,
-    MatSidenavModule,
-    MatListModule,
-    MatCardModule,
-    MatButtonModule,
-    MatSnackBarModule,
-    MatFormFieldModule,
-    MatInputModule,
-  ],
-  providers: [
-    // services
-  ],
-  bootstrap: [AppComponent]
+		// material
+		MatToolbarModule,
+		MatSidenavModule,
+		MatListModule,
+		MatCardModule,
+		MatButtonModule,
+		MatSnackBarModule,
+		MatFormFieldModule,
+		MatInputModule,
+		MatTableModule,
+		MatPaginatorModule,
+		MatSortModule,
+	],
+	providers: [
+		{ provide: LOCALE_ID, useValue: 'pt-BR' }
+	],
+	bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
---
+++ frontend/src/app/components/product/product-read/product-read.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { Product } from '../../../models/product.model';
+import { ProductService } from '../../../services/rest/product.service';
+
+@Component({
+	selector: 'app-product-read',
+	templateUrl: './product-read.component.html',
+	styleUrls: ['./product-read.component.scss']
+})
+export class ProductReadComponent implements OnInit {
+
+	products: Product[];
+	displayedColumns = ['id', 'name', 'price'];
+
+	constructor(
+		private productService: ProductService
+	) {
+	}
+
+	ngOnInit(): void {
+		this.productService.read().subscribe(products => {
+			this.products = products;
+		});
+	}
+
+}
